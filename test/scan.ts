76c81736d354ff50c18a3350a30c3792c12998e6
狼叔 <i5ting@126.com>
2022-09-29 17:13:56 +0800

    refact, please ignore

--- test/scan.ts
+++ test/scan.ts
@@ -2,12 +2,21 @@ import { test } from 'uvu';
 import * as path from 'node:path'
 import * as assert from 'uvu/assert';
 
-import { getTsFiles } from "../src/loadObject/scan";
+import { getTsFiles, requireDir } from "../src/";
 
 test('getTsFiles(dir)', () => {
   const dir = path.resolve(process.cwd(), './tests/')
 
   const files = getTsFiles(dir)
+  assert.is(Object.keys(files).length, 4)
+})
+
+test.only('requireDir(dir)', () => {
+  const dir = path.resolve(process.cwd(), './tests/')
+
+  const files = requireDir(dir, {})
+
+  console.dir(files)
   assert.is(Object.keys(files).length, 4)
 })
 
