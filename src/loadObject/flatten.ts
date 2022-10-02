c31ca8b30c12c6814168af60fde119fc1d54b882
i5ting <i5ting@126.com>
2022-10-02 23:13:22 +0800

    refact, please ignore

--- src/loadObject/flatten.ts
+++ src/loadObject/flatten.ts
@@ -1,59 +1,56 @@
-import { Debug } from '../Utils'
+import { Debug } from "../Utils";
 
-const debug = Debug()
+const debug = Debug();
 
-export function flatten(node: any, path = '', nodeList = []) {
-    if (node != null) {
-        debug('node i')
+export function flatten(node: any, path = "", nodeList = []) {
+  if (node != null) {
+    debug("node i");
 
-        // let children = node.children
-        for (let i in node) {
-            debug(i)
-            debug(node[i])
+    // let children = node.children
+    for (let i in node) {
+      debug(i);
+      debug(node[i]);
 
-            if (node[i]['newClz']) {
-                debug('找到了具体的object了 ' + path)
+      if (node[i]["newClz"]) {
+        debug("找到了具体的object了 " + path);
 
-                node[i]['path'] = path + '/' + i
-                nodeList.push(node[i]);
-            } else {
-                debug('没找到，继续深度优先' + i)
-                flatten(node[i], path + '/' + i, nodeList)
-            }
-        }
+        node[i]["path"] = path + "/" + i;
+        nodeList.push(node[i]);
+      } else {
+        debug("没找到，继续深度优先" + i);
+        flatten(node[i], path + "/" + i, nodeList);
+      }
     }
+  }
 
-    return nodeList
+  return nodeList;
 }
 
 // Declare a flatten function that takes
 // object as parameter and returns the
 // flatten object
-export function flattenObj(ob) {
+export function flattenObj(ob: any) {
+  // The object which contains the
+  // final result
+  let result = {};
 
-    // The object which contains the
-    // final result
-    let result = {};
-
-    // loop through the object "ob"
-    for (const i in ob) {
-
-        // We check the type of the i using
-        // typeof() function and recursively
-        // call the function again
-        if ((typeof ob[i]) === 'object' && !Array.isArray(ob[i])) {
-            const temp = flattenObj(ob[i]);
-            for (const j in temp) {
-
-                // Store temp in result
-                result[i + '.' + j] = temp[j];
-            }
-        }
-
-        // Else store ob[i] in result directly
-        else {
-            result[i] = ob[i];
-        }
+  // loop through the object "ob"
+  for (const i in ob) {
+    // We check the type of the i using
+    // typeof() function and recursively
+    // call the function again
+    if (typeof ob[i] === "object" && !Array.isArray(ob[i])) {
+      const temp = flattenObj(ob[i]);
+      for (const j in temp) {
+        // Store temp in result
+        result[i + "." + j] = temp[j];
+      }
     }
-    return result;
+
+    // Else store ob[i] in result directly
+    else {
+      result[i] = ob[i];
+    }
+  }
+  return result;
 }
--- src/loadObject/scan.ts
+++ src/loadObject/scan.ts
@@ -1,117 +1,111 @@
-import { flattenObj } from './flatten'
+import { flattenObj, Debug, getDataMapping, requireDir } from "../";
 
-import { Debug } from '../Utils'
+const debug = Debug();
 
-import { getDataMapping } from '../'
-
-import { requireDir } from './require'
-
-const debug = Debug()
-
-var cache = {}
+var cache = {};
 
 export function getTsFiles(dir: string) {
-    // 定制require-dir
-    var Classes = requireDir(dir, {
-        recurse: true,
-        extensions: ['.ts'],
-        require: function () { }
-    })
-    debug(Classes)
+  // 定制require-dir
+  var Classes = requireDir(dir, {
+    recurse: true,
+    extensions: [".ts"],
+    require: function () {},
+  });
+  debug(Classes);
 
-    return flattenObj(Classes)
+  return flattenObj(Classes);
 }
 
 export function load(file: string) {
-    var Clazz = require(`${file}`)
-    var obj = new Clazz.default()
-    var clz_name = obj.constructor.name
-    const data = getDataFromDecoratorJson(file, obj)
-    if (!data) return
+  var Clazz = require(`${file}`);
+  var obj = new Clazz.default();
+  var clz_name = obj.constructor.name;
+  const data = getDataFromDecoratorJson(file, obj);
+  if (!data) return;
 
-    // Clazz.default.data = data
-    // require('../decrator').emptydata()
-    cache = {}
+  // Clazz.default.data = data
+  // require('../decrator').emptydata()
+  cache = {};
 
-    obj.__data = data
+  obj.__data = data;
 
-    var newClz = data[clz_name] //|| {}
-    if (newClz) newClz.__obj = obj
+  var newClz = data[clz_name]; //|| {}
+  if (newClz) newClz.__obj = obj;
 
-    return { clz_name, newClz }
+  return { clz_name, newClz };
 }
 
 function getDataFromDecoratorJson(file: string, obj: object) {
-    // getEableRunDataMapping
-    // [
-    //     { method: 'initAll', hook: 'BeforeAll' },
-    //     { method: 'init', hook: 'BeforeEach' },
-    //     { method: 'tearDown', hook: 'AfterEach' },
-    //     { method: 'tearDownAll', hook: 'AfterAll' },
-    //     { method: 'succeedingTest', test: 'Test' },
-    //     { method: 'addition', test: 'Test' },
-    //     { Class: 'MyFirstJUnitJupiterTests', DisplayName: 'Clz test case' }
-    //  ]
+  // getEableRunDataMapping
+  // [
+  //     { method: 'initAll', hook: 'BeforeAll' },
+  //     { method: 'init', hook: 'BeforeEach' },
+  //     { method: 'tearDown', hook: 'AfterEach' },
+  //     { method: 'tearDownAll', hook: 'AfterAll' },
+  //     { method: 'succeedingTest', test: 'Test' },
+  //     { method: 'addition', test: 'Test' },
+  //     { Class: 'MyFirstJUnitJupiterTests', DisplayName: 'Clz test case' }
+  //  ]
 
-    // getDataMapping
-    // [
-    //     { method: 'initAll', hook: 'BeforeAll' },
-    //     { method: 'init', hook: 'BeforeEach' },
-    //     { method: 'tearDown', hook: 'AfterEach' },
-    //     { method: 'tearDownAll', hook: 'AfterAll' },
-    //     { method: 'succeedingTest', test: 'Test' },
-    //     { method: 'addition', test: 'Test' },
-    //     {
-    //       method: 'addition5',
-    //       test: 'Test',
-    //       DisplayName: 'Custom test name containing spaces222',
-    //       Disabled: 'Disabled until bug #42 has been resolved'
-    //     },
-    //     { Class: 'MyFirstJUnitJupiterTests', DisplayName: 'Clz test case' }
-    //   ]
-    const data = getDataMapping(file)
-    const clazz = data.find(item => item['Class']?.length > 0)
+  // getDataMapping
+  // [
+  //     { method: 'initAll', hook: 'BeforeAll' },
+  //     { method: 'init', hook: 'BeforeEach' },
+  //     { method: 'tearDown', hook: 'AfterEach' },
+  //     { method: 'tearDownAll', hook: 'AfterAll' },
+  //     { method: 'succeedingTest', test: 'Test' },
+  //     { method: 'addition', test: 'Test' },
+  //     {
+  //       method: 'addition5',
+  //       test: 'Test',
+  //       DisplayName: 'Custom test name containing spaces222',
+  //       Disabled: 'Disabled until bug #42 has been resolved'
+  //     },
+  //     { Class: 'MyFirstJUnitJupiterTests', DisplayName: 'Clz test case' }
+  //   ]
+  const data = getDataMapping(file);
+  const clazz = data.find((item) => item["Class"]?.length > 0);
 
-    var className = clazz['Class']
-    var classDisplayName = clazz['DisplayName']
+  var className = clazz["Class"];
+  var classDisplayName = clazz["DisplayName"];
 
-    if (!cache[className]) cache[className] = {}
-    if (!cache[className]['hook']) cache[className]['hook'] = {}
+  if (!cache[className]) cache[className] = {};
+  if (!cache[className]["hook"]) cache[className]["hook"] = {};
 
-    data.forEach(function (item) {
-        if (item['method']) {
-            const propertyName = item['method']
+  data.forEach(function (item) {
+    if (item["method"]) {
+      const propertyName = item["method"];
 
-            if (!cache[className][propertyName]) cache[className][propertyName] = {}
+      if (!cache[className][propertyName]) cache[className][propertyName] = {};
 
-            if (item['hook'] === 'BeforeAll') {
-                cache[className]['hook']['before'] = obj[item['method']]
-            }
-            if (item['hook'] === 'BeforeEach') {
-                cache[className]['hook']['before.each'] = obj[item['method']]
-            }
-            if (item['hook'] === 'AfterEach') {
-                cache[className]['hook']['after.each'] = obj[item['method']]
-            }
-            if (item['hook'] === 'AfterAll') {
-                cache[className]['hook']['after'] = obj[item['method']]
-            }
+      if (item["hook"] === "BeforeAll") {
+        cache[className]["hook"]["before"] = obj[item["method"]];
+      }
+      if (item["hook"] === "BeforeEach") {
+        cache[className]["hook"]["before.each"] = obj[item["method"]];
+      }
+      if (item["hook"] === "AfterEach") {
+        cache[className]["hook"]["after.each"] = obj[item["method"]];
+      }
+      if (item["hook"] === "AfterAll") {
+        cache[className]["hook"]["after"] = obj[item["method"]];
+      }
 
-            if (item['test']) {
-                if (item['DisplayName']) {
-                    cache[className][propertyName]['desc'] = item['DisplayName']
-                } else {
-                    cache[className][propertyName]['desc'] = 'no display name'
-                }
-
-                if (item['Disabled']) {
-                    cache[className][propertyName]['skip'] = true
-                    cache[className][propertyName]['skipReason'] = item['Disabled']
-                }
-                cache[className][propertyName]['fn'] = obj[item['method']]
-            }
+      if (item["test"]) {
+        if (item["DisplayName"]) {
+          cache[className][propertyName]["desc"] = item["DisplayName"];
+        } else {
+          cache[className][propertyName]["desc"] = "no display name";
         }
-    })
 
-    return cache
+        if (item["Disabled"]) {
+          cache[className][propertyName]["skip"] = true;
+          cache[className][propertyName]["skipReason"] = item["Disabled"];
+        }
+        cache[className][propertyName]["fn"] = obj[item["method"]];
+      }
+    }
+  });
+
+  return cache;
 }
