74fca94575dea3b1e6f04172752a857a9bc0a157
i5ting <i5ting@126.com>
2022-10-10 21:46:19 +0800

    refact: move tests to examples

--- api.ts
+++ api.ts
@@ -1,11 +1,11 @@
 import * as path from "node:path";
 import { run } from "./src";
 
-let b = path.resolve(process.cwd(), "./tests/");
+let b = path.resolve(process.cwd(), "./examples/");
 // runTests([f]);
 
-let a = path.resolve(process.cwd(), "./tests/test-class-disable.ts");
-let f = path.resolve(process.cwd(), "./tests/test.ts");
+let a = path.resolve(process.cwd(), "./examples/test-class-disable.ts");
+let f = path.resolve(process.cwd(), "./examples/test.ts");
 // runTestFile([f]);
 // run([a, f]);
 
