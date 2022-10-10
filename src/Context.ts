a44cdacba331da8c4c68b815b0e774133e935996
i5ting <i5ting@126.com>
2022-10-10 20:49:40 +0800

    fix: remove unused runTest method in context.ts

--- src/Context.ts
+++ src/Context.ts
@@ -1,13 +1,10 @@
-import * as fs from 'node:fs'
 import * as Promise2 from "bluebird";
-import { loadFromDecorator, loadFromCache } from "./loadObject/scan";
+import { loadFromCache } from "./loadObject/scan";
 import { Debug } from "./Utils";
 
-let debug = Debug("ts-junit");
-// debug = console.dir;
+const debug = Debug("ts-junit");
 
 import IStrategy from "./IStrategy";
-import { assert } from "node:console";
 
 /**
  * The Context defines the interface of interest to clients.
@@ -35,52 +32,6 @@ export default class Context {
     this.strategy = strategy;
   }
 
-  /**
-   * run one test file
-   *
-   * parse ts to get decorator meta data
-   * invoke js to run test
-   */
-  public runTest(tsFile: string, jsFile: string): void {
-    debug("runTest: " + jsFile);
-
-    assert(fs.existsSync(jsFile));
-
-    debug(" --- runTest --- ");
-    let nodeList = [loadFromDecorator(jsFile)];
-
-    debug("--------------- MAIN ------------------");
-    debug("nodeList");
-    debug(nodeList);
-
-    for (let i in nodeList) {
-      const Clazz = nodeList[i];
-      debug("Clazz---");
-      debug(Clazz);
-
-      let newClz = Clazz.newClz;
-
-      debug(newClz);
-
-      var obj = Clazz.newClz.__obj;
-      delete newClz.__obj;
-
-      debug("Context: Run tests using the strategy (not sure how it'll do it)");
-
-      this.strategy.testcase(Clazz.clz_name);
-
-      //   let d = console.dir;
-      //   d(i);
-      //   d(Clazz.clz_name);
-      //   d(newClz);
-      //   d(obj);
-
-      this.strategy.parseData(i, Clazz.clz_name, newClz, obj);
-
-      this.strategy.test.run();
-    }
-  }
-
   public runTsTestFiles(files: string[]): any {
     files = files.map(function (file) {
       return file.replace(".ts", "");
@@ -91,8 +42,6 @@ export default class Context {
   }
 
   private _runTsTestFile(file: string): any {
-    debug(" --- runTests --- ");
-    // console.dir("load ts file=" + file);
     let that = this;
 
     debug(" --- runTest --- ");
