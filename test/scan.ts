748a86eff72aa0fd17c489f3c27b397d4241ad17
Jason <m.jason.liu@outlook.com>
2022-10-11 17:09:49 +0800

    chores: prettier config (#20)
    * chores: prettier config
    
    * add prettier to recommand extensions
    * prettier config is now required
    * single quotes to double quotes
    * more specifically what files will be formatted by prettier
    * add prettier check for later CI uses
    
    * ci: use foramt:check

--- test/scan.ts
+++ test/scan.ts
@@ -1,22 +1,22 @@
-import { test } from 'uvu';
-import * as path from 'node:path';
-import * as assert from 'uvu/assert';
+import { test } from "uvu";
+import * as path from "node:path";
+import * as assert from "uvu/assert";
 
-import { getTsFiles, requireDir } from '../src/';
+import { getTsFiles, requireDir } from "../src/";
 
-test('getTsFiles(dir)', () => {
-  const dir = path.resolve(process.cwd(), './examples/');
+test("getTsFiles(dir)", () => {
+  const dir = path.resolve(process.cwd(), "./examples/");
 
   const files = getTsFiles(dir);
   assert.is(Object.keys(files).length, 4);
 });
 
-test.only('requireDir(dir)', () => {
-  const dir = path.resolve(process.cwd(), './examples/');
+test.only("requireDir(dir)", () => {
+  const dir = path.resolve(process.cwd(), "./examples/");
 
   const files = requireDir(dir, {
     recurse: true,
-    extensions: ['.ts'],
+    extensions: [".ts"],
     require: function () {},
   });
 
