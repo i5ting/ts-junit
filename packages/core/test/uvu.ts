7ff9a84e4a1f07ab33317479da13b36e05b17c52
狼叔 <i5ting@126.com>
2022-10-18 14:39:27 +0800

    chore: rm test/scan.ts in core package

--- packages/core/test/uvu.ts
+++ packages/core/test/uvu.ts
@@ -2,39 +2,10 @@ import { test } from "uvu";
 import * as path from "node:path";
 import * as assert from "uvu/assert";
 
-import { getAllImportsForFile, getNeedCompileFiles } from "../src/";
-
 test("Math.sqrt()", () => {
   assert.is(Math.sqrt(4), 2);
   assert.is(Math.sqrt(144), 12);
   assert.is(Math.sqrt(2), Math.SQRT2);
-});
-
-test.only("getAllImportsForFile()", () => {
-  const testFile = process.cwd() + "/examples/test";
-
-  let testTsFile = testFile;
-  // let testJsFile = testFile;
-
-  // const a = getDependencyImports(['./tests/test.ts','./tests/a/test2.ts'])
-  const extension = path.extname(testFile);
-  if (!extension) {
-    testTsFile += ".ts";
-    // testJsFile += ".js";
-
-    testTsFile = testTsFile.replace(process.cwd() + "/", "");
-
-    // testJsFile =
-    //   path.resolve(process.cwd(), "./") +
-    //   "/output" +
-    //   testJsFile.replace(process.cwd(), "");
-  }
-
-  getAllImportsForFile(testTsFile, { verbose: false });
-
-  const needCompileFiles = getNeedCompileFiles();
-
-  assert.is(needCompileFiles.length, 2);
 });
 
 test("JSON", () => {
