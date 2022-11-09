af1af24ab161e70f86cf86c993553fe4a007bdec
Hulk <33158585+byteHulk@users.noreply.github.com>
2022-11-09 21:51:25 +0800

    refactor: uvu.ts parseData fun

--- packages/strategy/src/uvu.ts
+++ packages/strategy/src/uvu.ts
@@ -23,47 +23,45 @@ export class UvuStrategy implements Strategy {
   ): void {
     if (Clazz["skipClass"]) {
       console.warn(`Class skip ${clz_name} reason: ${Clazz.skipClassReason}`);
-    } else {
-      for (const j in Clazz) {
-        if (j === "hook") {
-          for (const z in Clazz["hook"]) {
-            debug(z + " hook=" + Clazz["hook"][z]);
+      return;
+    }
 
-            if (z.split(".").length == 2) {
-              const a = z.split(".");
+    for (const j in Clazz) {
+      if (j === "hook") {
+        for (const z in Clazz["hook"]) {
+          debug(z + " hook=" + Clazz["hook"][z]);
 
-              debug("a1= " + a);
+          if (z.split(".").length == 2) {
+            const a = z.split(".");
 
-              this.test[a[0]][a[1]](Clazz["hook"][z].bind(obj));
-            } else {
-              debug(z + " a2= " + Clazz["hook"][z]);
-              // console.dir("z")
-              // console.dir(Clazz['hook'][z])
-              // console.dir(z)
-              this.test[z](Clazz["hook"][z].bind(obj));
-            }
-          }
-        } else {
-          if (!Clazz[j]["skip"]) {
-            debug(`define testcase ${j}`);
-            debug(" test.handler = " + obj[j]);
+            debug("a1= " + a);
 
-            if (Clazz[j]["params"]) {
-              const params = Clazz[j]["params"];
-
-              params.map((param) => {
-                this.test(j, obj[j].bind(obj, param));
-              });
-            } else {
-              this.test(j, obj[j].bind(obj));
-            }
+            this.test[a[0]][a[1]](Clazz["hook"][z].bind(obj));
           } else {
-            debug(`skipReason ${j}`);
-            console.log(
-              `method skip ${Clazz[i]}#${j}() reason: ${Clazz[j]["skipReason"]}`,
-            );
+            debug(z + " a2= " + Clazz["hook"][z]);
+
+            this.test[z](Clazz["hook"][z].bind(obj));
           }
         }
+      } else if (Clazz[j]["skip"]) {
+        debug(`skipReason ${j}`);
+        console.log(
+          `method skip ${Clazz[i]}#${j}() reason: ${Clazz[j]["skipReason"]}`,
+        );
+      } else if (Clazz[j]["params"]) {
+        debug(`define params testcase ${j}`);
+        debug("test.handler = " + obj[j]);
+
+        const params = Clazz[j]["params"];
+
+        params.map((param) => {
+          this.test(j, obj[j].bind(obj, param));
+        });
+      } else {
+        debug(`define testcase ${j}`);
+        debug("test.handler = " + obj[j]);
+
+        this.test(j, obj[j].bind(obj));
       }
     }
   }
